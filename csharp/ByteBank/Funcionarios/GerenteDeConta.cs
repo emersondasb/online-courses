80924acbe9a10c0775720aacfd616233987bafcd
Emerson Bezerra <emersondasb@outlook.com>
2020-08-08 11:56:54 -0300

    Interfaces em C#

--- csharp/ByteBank/Funcionarios/GerenteDeConta.cs
+++ csharp/ByteBank/Funcionarios/GerenteDeConta.cs
@@ -1,3 +1,4 @@
+using ByteBank.Sistemas;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ByteBank.Funcionarios
 {
-    public class GerenteDeConta : Funcionario
+    public class GerenteDeConta : FuncionarioAutenticavel
     {
         public GerenteDeConta(string cpf) : base(4000, cpf)
         {
