170e92f4745d9bb25a569714ebe1fc1b2f573eda
Emerson Bezerra <emersondevelops@gmail.com>
2020-08-12 17:40:12 -0300

    Using nuget packages

--- csharp/libraries-and-documentation/ByteBank.SistemaAgencia/Program.cs
+++ csharp/libraries-and-documentation/ByteBank.SistemaAgencia/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ByteBank.Modelos;
 using ByteBank.Modelos.Funcionarios;
+using Humanizer;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -12,14 +14,21 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(847, 489754);
-            Funcionario funcionario = null;
-            Console.WriteLine(conta.Numero);
+            DateTime dataFimPagamento = new DateTime(2020, 10, 17);
+            DateTime dataCorrente = DateTime.Now;
 
-            new ContaCorrente(654, 849845646);
+            TimeSpan diferenca = dataFimPagamento - dataCorrente;
+
+            string mensagem = TimeSpanHumanizeExtensions.Humanize(diferenca);
+
+            Console.WriteLine(dataCorrente);
+            Console.WriteLine(dataFimPagamento);
+            Console.WriteLine($"Vencimento em {mensagem}.");
 
 
             Console.ReadLine();
         }
+
+        
     }
 }
