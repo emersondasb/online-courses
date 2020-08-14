39478e4feab055623a646ac5efbbd2881adb44ef
Emerson Bezerra <emersondevelops@gmail.com>
2020-08-13 21:25:07 -0300

    Substring method

---
+++ csharp/string-and-regex/Strings/Strings/ExtratorValorDeArgumentos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strings
+{
+    public class ExtratorValorDeArgumentos
+    {
+        private readonly string _argumentos;
+        public string URL { get; }
+        public ExtratorValorDeArgumentos(string url)
+        {
+            if (String.IsNullOrEmpty(url))
+            {
+                throw new ArgumentException("O argumento url não pode ser vazio nem nulo.", nameof(url));
+            }
+            
+            int indiceInterrogacao = url.IndexOf('?');
+            _argumentos = url.Substring(indiceInterrogacao + 1);
+
+            URL = url;
+        }
+
+        public string GetValor(string nomeParametro)
+        {
+            int indiceParametro = _argumentos.IndexOf();
+            return _argumentos;
+        }
+    }
+}
---
+++ csharp/string-and-regex/Strings/Strings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string url = "pagina?argumento";
+
+            int indiceInterrogacao = url.IndexOf('?');
+            
+            string argumento = url.Substring(++indiceInterrogacao);
+
+            Console.WriteLine(url);
+            Console.WriteLine(argumento);
+
+            Console.ReadLine();
+        }
+    }
+}
